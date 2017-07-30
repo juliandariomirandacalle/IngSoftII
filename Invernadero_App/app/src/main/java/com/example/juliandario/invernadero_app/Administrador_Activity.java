7d87a4cec2e416925d4236e37efb4b50648eb9f2
Julián Darío Miranda <Julin Daro Miranda>
2017-07-30 10:40:04 -0500

    Versión con interfaces para admin y op mejoradas en estetica

--- Invernadero_App/app/src/main/java/com/example/juliandario/invernadero_app/Administrador_Activity.java
+++ Invernadero_App/app/src/main/java/com/example/juliandario/invernadero_app/Administrador_Activity.java
@@ -1,13 +1,40 @@
 package com.example.juliandario.invernadero_app;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Administrador_Activity extends AppCompatActivity {
+
+    Spinner spinner_insumo_admin, spinner_proveedores_admin;
+    EditText edtxt_fecha_admin, edtxt_cantidad_admin;
+    Button btnEnviarAdmin, btnRegresarAdmin;
+    Context context = Administrador_Activity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrador_);
+
+        spinner_insumo_admin = (Spinner) findViewById(R.id.spinner_insumo_admin);
+        spinner_proveedores_admin = (Spinner) findViewById(R.id.spinner_proveedores_admin);
+        edtxt_fecha_admin = (EditText) findViewById(R.id.edtxt_fecha_admin);
+        edtxt_cantidad_admin = (EditText) findViewById(R.id.edtxt_cantidad_admin);
+        btnEnviarAdmin = (Button) findViewById(R.id.btnEnviarAdmin);
+        btnRegresarAdmin = (Button) findViewById(R.id.btnRegresarAdmin);
+
+        btnRegresarAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+                Administrador_Activity.this.finish();
+            }
+        });
     }
 }
--- Invernadero_App/app/src/main/java/com/example/juliandario/invernadero_app/Operario_Activity.java
+++ Invernadero_App/app/src/main/java/com/example/juliandario/invernadero_app/Operario_Activity.java
@@ -1,13 +1,40 @@
 package com.example.juliandario.invernadero_app;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Operario_Activity extends AppCompatActivity {
+
+    Spinner spinner_insumo_op, spinner_proveedores_op;
+    EditText edtxt_fecha_op, edtxt_cantidad_op;
+    Button btnEnviarOp, btnRegresarOp;
+    Context context = Operario_Activity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operario_);
+
+        spinner_insumo_op = (Spinner) findViewById(R.id.spinner_insumo_op);
+        spinner_proveedores_op = (Spinner) findViewById(R.id.spinner_proveedores_op);
+        edtxt_fecha_op = (EditText) findViewById(R.id.edtxt_fecha_op);
+        edtxt_cantidad_op = (EditText) findViewById(R.id.edtxt_cantidad_op);
+        btnEnviarOp = (Button) findViewById(R.id.btnEnviarOp);
+        btnRegresarOp = (Button) findViewById(R.id.btnRegresarOp);
+
+        btnRegresarOp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+                Operario_Activity.this.finish();
+            }
+        });
     }
 }
